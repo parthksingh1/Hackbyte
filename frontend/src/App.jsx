c1f6f372bdd95dfc1ed11e2ba233aa72a6f96756
Kushagra <22bcs148@iiitdmj.ac.in>
2024-04-06 04:58:31 +0530

    made the poll functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx';
+import Home from './pages/Home.jsx'
+import Poll from './components/Poll/Poll.jsx'
 import Navbar from './components/navbar.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-</BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/poll" element={<Poll />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-200 p-4 flex items-center justify-between">
 
       <div className="text-gray-800 font-bold text-xl">Alone</div>
-      
+
       <ul className="flex space-x-8">
-        <li><a href="#" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">Home</a></li>
-        <li><a href="#" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">Polls</a></li>
-        <li><a href="#" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">Confessions</a></li>
-        <li><a href="#" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">Settings</a></li>
+        <li>
+          <Link to="/" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/poll" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">
+            Poll
+          </Link>
+        </li>
+        <li>
+          <Link to="/confession" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">
+            Confession
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="text-lg text-gray-800 hover:text-gray-600 hover:border-yellow-500 border-b-2 border-transparent transition duration-300">
+            Settings
+          </Link>
+        </li>
       </ul>
-      
+
+
       <div className="flex items-center space-x-4">
         <input type="text" className="bg-gray-300 text-gray-800 p-2 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-500 hover:shadow-md" placeholder="Search..." />
         <div className="w-10 h-10 overflow-hidden rounded-full bg-gray-800 mb-4">
